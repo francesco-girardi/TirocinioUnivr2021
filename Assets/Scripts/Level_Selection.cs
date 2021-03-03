4b1b15190e3212c0c0197755335faf0a56bd6735
Francesco Girardi <keko.girardi@gmail.com>
2021-03-03 12:02:05 +0100

    Inizio di tutto

---
+++ Assets/Scripts/Level_Selection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Selection : MonoBehaviour
+{
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LevelTwo()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void RetutnToTown()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+  
+
+}
