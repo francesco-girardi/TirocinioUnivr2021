fdac41318d8344fbdbfa9a8ad8ab1bf3b56adee3
Francesco Girardi <keko.girardi@gmail.com>
2021-03-11 17:41:18 +0100

    Start Enemies code

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,11 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Enemy {
 
     public class EnemyController : MonoBehaviour {
 
+        /// <summary>
+        /// Enemy view distance
+        /// </summary>
+        [Header("Enemy properties")]
+        [Tooltip("Enemy view distance")]
+        public float lookRadius = 10;
+
+        private Transform target;
+
+        private NavMeshAgent agent;
+
+        private void Start() {
+            target = PlayerManager.Instance.playerObject.transform;
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update() {
+            float distance = Vector3.Distance(target.position, transform.position);
+
+            if (distance <= lookRadius)
+                agent.SetDestination(target.position);
+        }
+
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookRadius);
+        }
 
 
     }
