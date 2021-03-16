8511aeffb5a2c9f0d6a77c6cf6d4963f92953812
Francesco Girardi <keko.girardi@gmail.com>
2021-03-16 13:38:54 +0100

    Added sound manager

---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    [System.Serializable]
+    public class Sound {
+
+        [Header("Audio Info")]
+        [Tooltip("Sound name")]
+        public string soundName;
+
+        [Tooltip("audio clip to play")]
+        public AudioClip soundClip;
+
+        [Tooltip("Volume for this audio clip")]
+        [Range(0f, 1f)]
+        public float soundVolume;
+
+        [Tooltip("Pith for this audio clip")]
+        [Range(-3f, 3f)]
+        public float soundPith = 1f;
+
+        [Tooltip("Can this sound loop")]
+        public bool soundLoop;
+
+        [HideInInspector]
+        public AudioSource audioSource;
+    }
+
+}
---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Audio {
+
+    public class SoundManager : MonoBehaviour {
+
+        /// <summary>
+        /// <br>Instance of SoundManager give you access to all public methods.</br>
+        /// ( <b>There can only be one</b> )
+        /// </summary>
+        public SoundManager Instance { get; private set; }
+
+        /// <summary>
+        /// All game sounds
+        /// </summary>
+        [Header("Audio Info")]
+        [Tooltip("Array of game sounds")]
+        public Sound[] sounds;
+
+        /// <summary>
+        /// Find and play a sound in game
+        /// </summary>
+        /// <param name="soundName"></param>
+        public void PlaySound(string soundName) {
+            Sound s = System.Array.Find(sounds, sound => sound.soundName == soundName);
+
+            if (s == null) {
+                Debug.LogError("File " + s.soundName + " not found!");
+                return;
+            }
+
+            s.audioSource.Play();
+        }
+
+        private void Awake() {
+            if (Instance == null)
+                Instance = this;
+            else {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            foreach (Sound sound in sounds) {
+                sound.audioSource = gameObject.AddComponent<AudioSource>();
+                sound.audioSource.clip = sound.soundClip;
+                sound.audioSource.volume = sound.soundVolume;
+                sound.audioSource.pitch = sound.soundPith;
+                sound.audioSource.loop = sound.soundLoop;
+            }
+        }
+
+        private void Start() {
+            PlaySound("background");
+        }
+
+    }
+
+}
