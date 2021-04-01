de7168ce903a09bbfbb823f6cb32c601e3d8b3ea
Luca <luca07061998@gmail.com>
2021-04-01 17:50:06 +0200

    Aggiunta mesh portale e qualche effetto, modificato prefab player e thunder slime

---
+++ Assets/FDAttack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FDAttack : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Ball;
+
+    void ChargeSphere(){
+
+        Instantiate(Ball, transform.position, Quaternion.identity);
+        
+    }
+
+}
---
+++ Assets/PrepareAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrepareAttack : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject lightning;
+
+    [SerializeField]
+    private ParticleSystem discharge;
+
+
+
+    void ActivateLightning(){
+
+        lightning.SetActive(true);
+        
+    }
+
+    void DeactivateLightning() {
+
+        lightning.SetActive(false);
+    }
+
+    void ActivateDischarge(){
+
+        discharge.Play();
+        
+    }
+}
