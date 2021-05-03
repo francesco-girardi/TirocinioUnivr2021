4a27c84689ad4ac3da9720fbf5ee637b93b7de3f
Davide Furlani <davide.furlani0998@gmail.com>
2021-05-03 10:54:05 +0200

    (D) risistemata la camera

--- Assets/Scripts/Interactions/AlchemistShop.cs
+++ Assets/Scripts/Interactions/AlchemistShop.cs
@@ -2,43 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Interactions{
+namespace Interactions {
 
-public class AlchemistShop : MonoBehaviour
-{
-    public GameObject Camera;
+    public class AlchemistShop : MonoBehaviour {
+        private bool isShopping = false;
 
-    private bool isShopping = false;
-    public bool IsShopping{get=>isShopping;}
+        private PlayerLogic player;
 
-    private PlayerLogic player;
+        void Start() {
 
-    void Start(){
-        StartCoroutine(TargetPlayer());
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void OnTriggerStay(Collider other){
-        Vector3 posizione=new Vector3 (18.19f, 2.79f, 17.28f);
-
-        if (Input.GetKey(KeyCode.E) && isShopping==false){
-            Camera.transform.position=posizione;
-            isShopping = true;
         }
 
-        if(Input.GetKey(KeyCode.Escape) && isShopping==true){
-            isShopping = false;
+        void Update() {
+
         }
-    }
 
-    private IEnumerator TargetPlayer() {
-        yield return new WaitForSeconds(1);
+        void OnTriggerStay(Collider other) {
+            Vector3 posizione = new Vector3(18.19f, 2.79f, 17.28f);
 
-        player = PlayerManager.Instance.playerObject.GetComponent<PlayerLogic>();
+            if (Input.GetKey(KeyCode.E) && isShopping == false) {
+                isShopping = true;
+            }
+
+            if (Input.GetKey(KeyCode.Escape) && isShopping == true) {
+                isShopping = false;
+            }
+        }
+
+
+
     }
-}
 }
