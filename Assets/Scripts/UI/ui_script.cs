8388f5d384f051830daa7652b25b624cb2ad0db2
Davide Furlani <davide.furlani0998@gmail.com>
2021-05-06 11:58:39 +0200

    (D) aggiunto fps counter, sistemato main menu

--- Assets/Scripts/UI/ui_script.cs
+++ Assets/Scripts/UI/ui_script.cs
@@ -15,6 +15,12 @@ namespace UI {
 
         public TMP_Text healthText;
 
+        public TMP_Text fpsText;
+
+        public TMP_Text maxfps;
+
+        public TMP_Text minfps;
+
         GameObject player;
 
         PlayerLogic playerLogic;
@@ -25,12 +31,23 @@ namespace UI {
             // healthSlider.GetComponentInChildren<Slider>();
             // healthText.GetComponentInChildren<TMP_Text>();
 
+            StartCoroutine(InitMinMax());
+
             fill.color = gradient.Evaluate(1f);
             border.color = gradient.Evaluate(1f);
+
+           
+
+            InvokeRepeating("FpsUpdate", 0f, 0.3f);
         }
 
         // Update is called once per frame
         void Update() {
+            HealthUpdate();
+            FpsMaxMin();
+        }
+
+        void HealthUpdate(){
             int health = 0;
 
             if (playerLogic != null)
@@ -40,6 +57,28 @@ namespace UI {
             healthText.text = health.ToString();
             fill.color = gradient.Evaluate(healthSlider.normalizedValue);
             border.color = gradient.Evaluate(healthSlider.normalizedValue);
+        }
+
+        void FpsUpdate(){
+            int fps = (int)(1 /Time.unscaledDeltaTime);
+            fpsText.SetText(fps.ToString());
+        }
+
+        void FpsMaxMin(){
+            int fps = (int)(1 /Time.unscaledDeltaTime);
+            if(fps<int.Parse(minfps.text))
+                minfps.SetText(fps.ToString());
+            
+            if(fps>int.Parse(maxfps.text))
+                maxfps.SetText(fps.ToString());
+        }
+
+        private IEnumerator InitMinMax(){
+            yield return new WaitForSeconds(5f);
+
+            int fps = (int)(1 /Time.unscaledDeltaTime);
+            maxfps.SetText(fps.ToString());
+            minfps.SetText(fps.ToString());
         }
 
         private IEnumerator getTarget() {
