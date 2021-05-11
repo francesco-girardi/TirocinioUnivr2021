81075cf347e0480063383ebdc5ba7ae7a4298271
Luca <luca07061998@gmail.com>
2021-05-11 15:40:57 +0200

    Effetti e cose

---
+++ Assets/Scripts/Enemies/DissolvingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class DissolvingScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private VisualEffect VFXGraph;
+
+    [SerializeField]
+    private float dissolveRate = 0.02f;
+
+    [SerializeField]
+    private float refreshRate = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/Slime/Boss_Slime_aniController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Slime_aniController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator boss;
+
+    private int atkc = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      InvokeRepeating("AttackCount", 2.0f, 5.0f);  
+    }
+
+    void AttackCount(){
+
+
+        switch(atkc)
+        {
+            case 3:
+                boss.SetInteger("attackNumber", 3);
+                atkc = 1;
+                break;
+
+            case 2:
+                boss.SetInteger("attackNumber", 2);
+                atkc = 3;
+                break;
+
+            case 1:
+                boss.SetInteger("attackNumber", 1);
+                atkc = 2;
+                break;
+
+            default:
+                boss.SetInteger("attackNumber", 0);
+                break;
+        }
+    }
+
+    private void ResetCounter(){
+
+        boss.SetInteger("attackNumber", 0);
+    }
+}
